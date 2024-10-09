2185c27269cabda494cbdb765698f3a0e99ea0e9
sofia-pastor <sofiapastor93@gmail.com>
2024-10-09 22:33:44 +0100

    added submit handling and typewritter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+function generateRecommendation(event) {
+  event.preventDefault();
+  let userSkillLevel = document.querySelector("#skill-level").value;
+  let userHeight = document.querySelector("#height").value;
+  let userWeight = document.querySelector("#weight").value;
+  let wavesCondition = document.querySelector("#wave-conditions");
+  let surfStyle = document.querySelector("#surf-style");
+  let contentElement = document.querySelector("#content");
 
+  new Typewriter("#content", {
+    strings: "Your board should be...",
+    autoStart: true,
+    delay: 20,
+    cursor: null,
+  });
+}
+
+let surfboardFormElement = document.querySelector("#surfboard-form");
+surfboardFormElement.addEventListener("submit", generateRecommendation);

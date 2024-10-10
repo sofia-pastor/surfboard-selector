68fee50de37f6ef19637c432bbb9897298deb9f5
sofia-pastor <sofiapastor93@gmail.com>
2024-10-10 01:22:24 +0100

    updated html, css and js
    JS:
    . added displayRecomendation function
    . updated generateRecomendation function
    .added AI api and incorporated the AI in the project
    
    html:
    .added img to be displayed in displayRecommendation function
    . added required to select
    . added axios
    
    css:
    . styled content and img
    
    css:

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
+function displayRecommendation(response) {
+  new Typewriter("#content", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: null,
+  });
+
+  let h2 = document.querySelector("h2");
+  h2.remove();
+  let img = document.getElementById("imgDisplay");
+  img.src = "src/surf.jpg";
+}
+
 function generateRecommendation(event) {
   event.preventDefault();
   let userSkillLevel = document.querySelector("#skill-level").value;
   let userHeight = document.querySelector("#height").value;
   let userWeight = document.querySelector("#weight").value;
-  let wavesCondition = document.querySelector("#wave-conditions");
-  let surfStyle = document.querySelector("#surf-style");
-  let contentElement = document.querySelector("#content");
+  let wavesCondition = document.querySelector("#wave-conditions").value;
+  let surfStyle = document.querySelector("#surf-style").value;
 
-  new Typewriter("#content", {
-    strings: "Your board should be...",
-    autoStart: true,
-    delay: 20,
-    cursor: null,
-  });
+  let apiKey = "tb3fa10906d8aeb018ofce8736864bb2";
+  let context =
+    "You are a surf expert and have a lot of knowledge about it and the surfboards. Your mission is to give the best advice of which board should a person choose. Please provide a short suggestion, without a title, and make sure that your answer is in basic html format, in order to insert your answer in one or more <div>. In your answer don't give the initial '```html' neither the final '```'.";
+  let prompt = `Can you please suggest a surfboard for a ${userSkillLevel} surfer who is ${userHeight} cm tall, weighs ${userWeight} kg, typically surfs ${wavesCondition} waves, and prefers a ${surfStyle} style?`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecommendation);
+  let waiting = document.querySelector("#content");
+  waiting.innerHTML = "⌛ Searching for the best option for you...";
 }
 
 let surfboardFormElement = document.querySelector("#surfboard-form");
